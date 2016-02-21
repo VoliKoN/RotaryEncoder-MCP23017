79e14af8c4cda6d5ee65cfc0caf55ecbe24de1eb
VoliKoN <VoliKoN@users.noreply.github.com>
2016-02-21 21:41:08 +0300

    proper hardware setup

--- examples/SimplePollRotator/SimplePollRotator.ino
+++ examples/SimplePollRotator/SimplePollRotator.ino
@@ -12,12 +12,19 @@
 // The current position is printed on output when changed.
 
 // Hardware setup:
-// Attach a rotary encoder with output pins to A2 and A3.
+// Connect pin #12 of the expander to Analog 5 (i2c clock)
+// Connect pin #13 of the expander to Analog 4 (i2c data)
+// Connect pins #15, 16 and 17 of the expander to ground (address selection)
+// Connect pin #9 of the expander to 5V (power)
+// Connect pin #10 of the expander to ground (common ground)
+// Connect pin #18 through a ~10kohm resistor to 5V (reset pin, active low)
+
+// Attach a rotary encoder with output pins to Input #0 and Input #1.
 // The common contact should be attached to ground.
+
 #include <Wire.h>
 #include <Adafruit_MCP23017.h>
 #include <McpEncoder.h>
-
 
 // Setup a RoraryEncoder for pins A2 and A3:
 McpEncoder encoder(0, 1, 0);
